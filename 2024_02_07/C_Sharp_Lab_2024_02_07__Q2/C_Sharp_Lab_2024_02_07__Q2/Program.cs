12ed2e3235c90fe95495cb7f3696dd2a3db0eb02
Mithila001 <mithiladissanayakaofficial@gmail.com>
2024-02-13 12:41:45 +0530

    Q2

--- 2024_02_07/C_Sharp_Lab_2024_02_07__Q2/C_Sharp_Lab_2024_02_07__Q2/Program.cs
+++ 2024_02_07/C_Sharp_Lab_2024_02_07__Q2/C_Sharp_Lab_2024_02_07__Q2/Program.cs
@@ -10,6 +10,40 @@ namespace C_Sharp_Lab_2024_02_07__Q2
     {
         static void Main(string[] args)
         {
+            Deposite();
         }
+
+        public static void Deposite()
+        {
+            BanckAccount banckAccount = new BanckAccount();
+            Console.Write("Enter you'r account number: ");
+            string userInputAccount = Console.ReadLine();
+            banckAccount.accountNumber += InputValidate(userInputAccount);
+            Console.Write("Enter the deposite amount:");
+            string userInputBalance = Console.ReadLine();
+            banckAccount.accountBalance += InputValidate(userInputBalance);
+            Console.WriteLine("Current account balance is: "+ banckAccount.accountBalance);
+
+        }
+
+        public static int InputValidate(string input)
+        {
+            int valideInput;
+            if(int.TryParse(input, out valideInput))
+            {
+                return valideInput;
+            }
+            else
+            {
+                Console.WriteLine("Invalide Input!!");
+                return 0;
+            }
+        }
+    }
+
+    public class BanckAccount
+    {
+        public int accountNumber;
+        public int accountBalance;
     }
 }
