0b6cc12d9cb8a9295ffe7fe6dc80bea7ffd93ef0
Mithila001 <mithiladissanayakaofficial@gmail.com>
2024-02-13 12:09:57 +0530

    Q1

---
+++ 2024_02_07/C_Sharp_Lab_2024_02_07__Q1/C_Sharp_Lab_2024_02_07__Q1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Lab_2024_02_07__Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Books myBook = new Books();
+
+            myBook.title = "How to Sleep";
+            myBook.authur = "Mr.Sleep";
+
+            Console.WriteLine("Title:"+ myBook.title);
+            Console.WriteLine("Author:"+ myBook.authur);
+        }
+    }
+
+    public class Books
+    {
+        public string title;
+        public string authur;
+    }
+}
