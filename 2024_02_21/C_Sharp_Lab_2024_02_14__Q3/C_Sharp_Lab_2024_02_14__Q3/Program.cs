dd69be0403478bff7d993032b436e5532a5eb65f
Mithila001 <mithiladissanayakaofficial@gmail.com>
2024-02-21 12:12:48 +0530

    Q3

--- 2024_02_21/C_Sharp_Lab_2024_02_14__Q3/C_Sharp_Lab_2024_02_14__Q3/Program.cs
+++ 2024_02_21/C_Sharp_Lab_2024_02_14__Q3/C_Sharp_Lab_2024_02_14__Q3/Program.cs
@@ -10,6 +10,38 @@ namespace C_Sharp_Lab_2024_02_14__Q3
     {
         static void Main(string[] args)
         {
+            Product product1 = new Product(101, "Laptop", 800, 10);
+            Console.WriteLine("Product ID: " + product1.productID);
+            Console.WriteLine("Product Name: " + product1.productName);
+            Console.WriteLine("Product Price: " + product1.price);
+            Console.WriteLine("Product Qunatity: " + product1.quantityInStock);
+        }
+
+        
+    }
+
+    public class Product
+    {
+        public int productID { get; }
+        public string productName;
+        public double price;
+        public int quantityInStock;
+
+        public Product(int productID, string productName, double price, int quantityInStock)
+        {
+            this.productID = productID;
+            this.productName = productName;
+            this.price = price;
+            this.quantityInStock = quantityInStock;
+        }
+
+        public void addProduct()
+        {
+
+        }
+        public void BuyProduct()
+        {
+
         }
     }
 }
